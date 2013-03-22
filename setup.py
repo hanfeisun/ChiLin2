faef8d544095785166cb231f8162ca4bc175653f
Samleo@Shenglin-MeimatoiMac.local <Samleo@Shenglin-MeimatoiMac.local>
2013-03-22 14:38:31 +0800

    add conservation centers db

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='chilin2',
     version='0.1',
-    packages=['chilin2', 'chilin2.python_templates'],
+    packages=['chilin2', 'chilin2.function_template'],
     url='',
     license='MIT',
     author='Hanfei Sun, Shenglin Mei, Qian Qin ',
     author_email='',
     description='',
     scripts = ["chilin2/ChiLin2.py"],
-    requires=["pyflow"]
-
+    requires=["pyflow"],
+    package_data = {"chilin2" : ["db/ChiLinQC.db", "jinja_template/*.jinja2","db/*.txt"]}
 )
